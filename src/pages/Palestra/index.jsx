bf597b81987a386b44fb43cae4f965fd15766e41
Márcio <marciobwfilho98@gmail.com>
2020-07-17 16:14:47 -0300

    Ajustando Tela de Palestras

--- src/pages/Palestra/index.jsx
+++ src/pages/Palestra/index.jsx
@@ -2,90 +2,82 @@ import React from 'react';
 import InputMask from 'react-input-mask';
 import './styles.css';
 import logo from '../../assets/logo.png';
-import voltaricon from '../../assets/voltaricon.png';
 
 function Palestra() {
   return (
     <>
       <div id="container-palestra">
-        <div className="conteudo">
-          <div className="sair">
-            <img src={voltaricon} alt="voltar" />
-          </div>
+        <div id="main-palestra">
+          <img src={logo} alt="logo" />
 
-          <div id="main-palestra">
-            <img src={logo} alt="logo" />
+          <div className="palestra">
+            <h1>
+              Cadastro
+              <br /> de Palestras
+            </h1>
+            <form>
+              <div className="field">
+                <label>Título da Palestra: </label>
+                <input
+                  type="text"
+                  className="titulo-input"
+                  placeholder="Digite um título..."
+                />
+              </div>
 
-            <div className="palestra">
-              <h1>
-                Cadastro
-                <br /> de Palestras
-              </h1>
-              <form>
+              <div className="field-group">
                 <div className="field">
-                  <label>Título da Palestra: </label>
+                  <label>Nome do Palestrante: </label>
                   <input
                     type="text"
-                    className="titulo-input"
-                    placeholder="Digite um título..."
+                    className="nome-palestrante-input"
+                    placeholder="Digite o nome completo..."
                   />
-                </div>
-
-                <div className="field-group">
-                  <div className="field">
-                    <label>Nome do Palestrante: </label>
-                    <input
-                      type="text"
-                      className="nome-palestrante-input"
-                      placeholder="Digite o nome completo..."
-                    />
-                  </div>
-
-                  <div className="field">
-                    <label>Data: </label>
-                    <InputMask
-                      className="data-input"
-                      mask="99/99/9999"
-                      maskPlaceholder="dd/mm/yyyy"
-                      placeholder="dd/mm/yyyy"
-                    />
-                  </div>
-
-                  <div className="field">
-                    <label>Hora: </label>
-                    <InputMask
-                      className="hora-input"
-                      placeholder="00:00"
-                      mask="99:99"
-                      maskPlaceholder="hh:mm"
-                    />
-                  </div>
-
-                  <div className="field">
-                    <label>Sala:</label>
-                    <input
-                      type="text"
-                      className="sala-input"
-                      placeholder="Ex: 202"
-                    />
-                  </div>
                 </div>
 
                 <div className="field">
-                  <label>Descrição do Palestrante:</label>
-                  <textarea
-                    className="descricao-input"
-                    placeholder="Descreva o palestrante..."
+                  <label>Data: </label>
+                  <InputMask
+                    className="data-input"
+                    mask="99/99/9999"
+                    maskPlaceholder="dd/mm/yyyy"
+                    placeholder="dd/mm/yyyy"
                   />
                 </div>
 
-                <div className="button">
-                  <button type="submit">Cadastrar</button>
+                <div className="field">
+                  <label>Hora: </label>
+                  <InputMask
+                    className="hora-input"
+                    placeholder="00:00"
+                    mask="99:99"
+                    maskPlaceholder="hh:mm"
+                  />
                 </div>
-              </form>
-            </div>
+
+                <div className="field">
+                  <label>Sala:</label>
+                  <input
+                    type="text"
+                    className="sala-input"
+                    placeholder="Ex: 202"
+                  />
+                </div>
+              </div>
+
+              <div className="field">
+                <label>Descrição do Palestrante:</label>
+                <textarea
+                  className="descricao-input"
+                  placeholder="Descreva o palestrante..."
+                />
+              </div>
+
+              <div className="button">
+                <button type="submit">Cadastrar</button>
+              </div>
+            </form>
           </div>
-          <div className="vazio" />
         </div>
       </div>
     </>
