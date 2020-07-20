5ac14d11e2f76e5c0db180bfcf97cd5a735c59fa
Márcio <marciobwfilho98@gmail.com>
2020-07-20 17:09:20 -0300

    Validando campos da tela de participantes

--- src/pages/Participante/index.jsx
+++ src/pages/Participante/index.jsx
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
+import * as yup from 'yup';
 import logo from '../../assets/logo.png';
 
+import api from '../../services/api';
+
 function Participante() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [universidade, setUniversidade] = useState('uepb');
+  const [curso, setCurso] = useState('');
+
+  const idAdmin = localStorage.getItem('id');
+
+  const dataSchema = yup.object().shape({
+    nome: yup.string().required(),
+    email: yup.string().required().email(),
+    senha: yup.string().required(),
+    universidade: yup.string().required(),
+    curso: yup.string().required(),
+  });
+
+  function handleInputNome(event) {
+    event.preventDefault();
+    const inputNome = event.target.value;
+    setNome(inputNome);
+  }
+
+  function handleInputEmail(event) {
+    event.preventDefault();
+    const inputEmail = event.target.value;
+    setEmail(inputEmail);
+  }
+
+  function handleInputSenha(event) {
+    event.preventDefault();
+    const inputSenha = event.target.value;
+    setSenha(inputSenha);
+  }
+
+  function handleInputUniversidade(event) {
+    event.preventDefault();
+    const inputUniversidade = event.target.value;
+    setUniversidade(inputUniversidade);
+  }
+
+  function handleInputCurso(event) {
+    event.preventDefault();
+    const inputCurso = event.target.value;
+    setCurso(inputCurso);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const dados = { nome, email, senha, universidade, curso };
+    if (!dataSchema.isValidSync(dados)) {
+      console.log('erro');
+    } else {
+      try {
+        const cabecalho = { id: idAdmin };
+        await api.post('/user', dados, { headers: cabecalho });
+        document.getElementById('formulario-participante').reset();
+      } catch (Error) {
+        console.log(Error.response.data.error);
+      }
+    }
+  }
+
   return (
     <>
       <div id="container-participante">
@@ -16,10 +81,15 @@ function Participante() {
               Cadastro
               <br /> de Participantes
             </h1>
-            <form>
+            <form id="formulario-participante">
               <div className="field">
                 <label htmlFor="nome">Nome: </label>
-                <input type="text" id="nome" placeholder="Aluno..." />
+                <input
+                  type="text"
+                  id="nome"
+                  placeholder="Aluno..."
+                  onChange={handleInputNome}
+                />
               </div>
               <div className="field-group">
                 <div className="field">
@@ -28,6 +98,7 @@ function Participante() {
                     type="email"
                     id="email-participante"
                     placeholder="Digite o Email..."
+                    onChange={handleInputEmail}
                   />
                 </div>
                 <div className="field">
@@ -36,15 +107,16 @@ function Participante() {
                     type="password"
                     id="senha-participante"
                     placeholder="Digite a Senha..."
+                    onChange={handleInputSenha}
                   />
                 </div>
               </div>
               <div className="field-group">
                 <div className="field">
                   <label htmlFor="universidade">Universidade: </label>
-                  <select id="universidade">
-                    <option>Uepb</option>
-                    <option>Outras</option>
+                  <select id="universidade" onChange={handleInputUniversidade}>
+                    <option>uepb</option>
+                    <option>outras</option>
                   </select>
                 </div>
                 <div className="field">
@@ -53,11 +125,14 @@ function Participante() {
                     type="text"
                     id="curso"
                     placeholder="Digite o Curso..."
+                    onChange={handleInputCurso}
                   />
                 </div>
               </div>
               <div className="button">
-                <button type="submit">Cadastrar</button>
+                <button type="submit" onClick={handleSubmit}>
+                  Cadastrar
+                </button>
               </div>
             </form>
           </div>
